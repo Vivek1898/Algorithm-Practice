864d179292cef1da9fb1f6ce80ee315a4938e363
Vivek Singh <viveksingh27795@gmail.com>
2022-01-25 02:14:41 +0530

    Winner in circle

---
+++ Recursion and Backtracking/26.Find the Winner of the Circular Game.cpp
@@ -0,0 +1,28 @@
+class Solution
+{
+public:
+    int findTheWinner(int n, int k)
+    {
+        queue<int> q;
+        for (int i = 0; i < n; i++)
+        {
+            q.push(i);
+        }
+        int count = 0;
+        while (q.size() > 1)
+        {
+            int val = q.front();
+            q.pop();
+            count++;
+            if (count == k)
+            {
+                count = 0;
+            }
+            else
+            {
+                q.push(val);
+            }
+        }
+        return q.front() + 1;
+    }
+};
