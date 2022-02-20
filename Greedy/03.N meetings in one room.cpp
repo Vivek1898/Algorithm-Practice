6919077d619ffab15fb20d4b29510fd7ffedc5f4
Vivek Singh <viveksingh27795@gmail.com>
2022-02-21 04:59:38 +0530

    Ovelapping Intervals Good approach

--- Greedy/03.N meetings in one room.cpp
+++ Greedy/03.N meetings in one room.cpp
@@ -1,3 +1,68 @@
+
+
+struct meeting { 
+	int start; 
+	int end; 
+	int pos; 
+}; 
+
+// Sorting meetings based on finish time
+static bool endSort(struct meeting m1, meeting m2) 
+{ 
+	return (m1.end < m2.end); 
+} 
+
+// Function for finding maximum meeting in one room 
+int maxMeetings(int s[], int f[], int n) 
+{ 
+	struct meeting meet[n];
+	int c = 1;
+	// Creating pairs of meeting(start,end) and position
+	for (int i = 0; i < n; i++) 
+	{ 
+		meet[i].start = s[i]; 
+		meet[i].end = f[i]; 
+		meet[i].pos = i + 1; 
+	} 
+
+	// Sorting of meetings in ascending order according to their finish time 
+	sort(meet, meet + n, endSort); 
+
+	// Vector for storing selected meeting. 
+	vector<int> m; 
+
+	// First meeting is always selected 
+	m.push_back(meet[0].pos); 
+ 
+	int prev_end = meet[0].end; 
+
+	// Checking if meeting can take place or not 
+	for (int i = 1; i < n; i++) { 
+		if (meet[i].start > prev_end) 
+		{ 
+			m.push_back(meet[i].pos); 
+			prev_end = meet[i].end; 
+			c++;
+		} 
+	} 
+	return c;
+}
+
+--------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  static bool compare( pair<int,int> &a, pair<int,int> &b){
 
         if(a.second<b.second)return true;
--- Greedy/04. Non-overlapping Intervals.cpp
+++ Greedy/04. Non-overlapping Intervals.cpp
@@ -1,5 +1,65 @@
 class Solution {
 public:
+
+    struct meeting { 
+	int start; 
+	int end; 
+	int pos; 
+}; 
+
+// Sorting meetings based on finish time
+static bool endSort(struct meeting m1, meeting m2) 
+{ 
+	return (m1.end < m2.end); 
+} 
+
+// Function for finding maximum meeting in one room 
+int eraseOverlapIntervals(vector<vector<int>>& intervals) 
+{ 
+   int n=intervals.size();
+	struct meeting meet[n];
+	int c = 1;
+	// Creating pairs of meeting(start,end) and position
+	for (int i = 0; i < n; i++) 
+	{ 
+		meet[i].start = intervals[i][0]; 
+		meet[i].end = intervals[i][1]; 
+		meet[i].pos = i + 1; 
+	} 
+
+	// Sorting of meetings in ascending order according to their finish time 
+	sort(meet, meet + n, endSort); 
+
+	// Vector for storing selected meeting. 
+	vector<int> m; 
+
+	// First meeting is always selected 
+	m.push_back(meet[0].pos); 
+ 
+	int prev_end = meet[0].end; 
+
+	// Checking if meeting can take place or not 
+	for (int i = 1; i < n; i++) { 
+		if (meet[i].start >= prev_end) 
+		{ 
+			m.push_back(meet[i].pos); 
+			prev_end = meet[i].end; 
+			c++;
+		} 
+	} 
+	return n-c;
+}
+
+    
+};
+
+
+----------------------------------------------------------------
+
+
+
+class Solution {
+public:
    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
         int n = intervals.size();
         
