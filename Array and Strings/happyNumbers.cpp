566363e6064cf7f52772f74b6c2b2aa560bd42da
Vivek Singh <viveksingh27795@gmail.com>
2022-05-29 22:39:41 +0530

    cycle

---
+++ Array and Strings/happyNumbers.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+  // This function finds our squared sum for the given value
+long long sqSum(long long num){
+	long long sum = 0;
+    while(num){
+		sum = sum + (num % 10) * (num % 10);
+        num /= 10;
+    }
+    return sum;
+}
+
+bool isHappy(int n){
+    int slow = n, fast = n;
+    do{
+        slow = sqSum(slow);  // One pointer increments by one change.
+        fast = sqSum(sqSum(fast));  // One pointer increments by two changes at once.
+		if(fast == 1)
+			return true;
+    }while(slow != fast);
+    
+    return false;
+}
+// This approach takes O(logn) time and O(1) space
+};
